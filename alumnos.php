75df85d6514290888969ae09c72a3794c79224f5
Anthony De La orden Garcia <anthonydelaordeng@gmail.com>
2025-06-23 10:57:37 +0200

    Create alumnos.php

---
+++ alumnos.php
@@ -0,0 +1,28 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Area del Alumno</title>
+</head>
+<body>
+    <main>
+    <h1>Área del Alumno</h1>
+    <p>Bienvenido al área del alumno. Aquí puedes gestionar tus datos y ver tu progreso.</p>
+    </main>
+    <div>
+        <a>Dame de Baja</a>
+        <a>Dame de Alta</a>
+        <a>Gestionar Datos</a>
+    </div>
+    
+    <footer>
+        <p>&copy; 2025 Proyecto</p>
+    </footer>
+
+    
+</body>
+</html>
+
