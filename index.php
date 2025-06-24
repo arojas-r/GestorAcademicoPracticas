497b33265c1db920482f5c65ebd8e1daf99c0cf4
Javier Cecilia Ruiz <laforjastore@gmail.com>
2025-06-24 10:25:21 +0200

    Añado index

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestión de Cursos</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+    <?php require "header.php"?>
+    <div class="breadcrumbs">
+        <a href="index.php">Inicio</a> &raquo; <span>Home</span>
+    </div>
+    <br>
+    <h1> &nbsp Bienvenid@ al área de administración del centro</h1>
+    <h3> &nbsp &nbsp &nbsp Usa el menu superior para acceder a las distintas secciones de trabajo</h3>
+    <?php require "footer.php"?>    
+</body>
+</html>
