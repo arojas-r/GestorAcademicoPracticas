6a2f858e35baff81bff570b4e1da66a6b707088b
Javier Cecilia Ruiz <laforjastore@gmail.com>
2025-06-25 17:51:37 +0200

    Delete 1practicas directory

--- 1practicas/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// config.php
-return [
-    'host' => 'localhost',
-    'db' => 'instituto',
-    'user' => 'root',
-    'pass' => '',
-    'charset' => 'utf8mb4'
-];
--- 1practicas/footer.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-    <link rel="stylesheet" href="estilos.css">
-
-    <footer>
-        &copy; <?php echo date("Y"); ?> Instituto de Educación Superior. Todos los derechos reservados.
-    </footer>
-
-</html>
