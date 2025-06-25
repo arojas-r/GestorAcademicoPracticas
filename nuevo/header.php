53c684ea33db6b7044fd4b6a44b3497824d5c536
alvlb97 <alvaro.lopezb97@gmail.com>
2025-06-25 12:06:22 +0200

    Add files via upload

---
+++ nuevo/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <header>
+    <link rel="stylesheet" href="estilos.css">
+
+        <div class="logo"><a href="index.php">📚 Instituto</a></div>
+        <nav>
+            <a href="alumnos.php">Alumnos</a>
+            <a href="profesores.php">Profesores</a>
+            <a href="cursos.php">Cursos</a>
+            <a href="matriculas.php">Matrículas</a>
+            <a href="consultas.php">Consultar</a>
+
+        </nav>
+    </header>
+</html>
