a4d583e59d95b6e8d8dc9170a4490a1c75d50106
alvlb97 <alvaro.lopezb97@gmail.com>
2025-06-25 12:07:58 +0200

    Add files via upload

---
+++ nuevo_alvaro/footer.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+    <link rel="stylesheet" href="estilos.css">
+
+    <footer>
+        &copy; <?php echo date("Y"); ?> Instituto de Educación Superior. Todos los derechos reservados.
+    </footer>
+
+</html>
