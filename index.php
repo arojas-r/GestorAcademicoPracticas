82ae3cd720db4c01b70c505b464b66cf75b7b798
Javier Cecilia Ruiz <laforjastore@gmail.com>
2025-06-25 17:35:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home INSTITUTO</title>
-    <link rel="stylesheet" href="css/instituto.css">
+    <link rel="stylesheet" href="instituto.css">
 </head>
 <?php require "header.php"; ?>
 
@@ -14,10 +14,12 @@
     <div class="breadcrumbs">
         <a href="index.php">Inicio</a> &raquo; <span>Home</span>
     </div>
-
+    <img class="backgroundIMG" src="https://pittcc.edu/wp-content/uploads/2023/04/Office-Administration-Background.jpg" alt="imagen de fondo">
     <main>
-        <h1>Bienvenido/a al área de administración del centro</h1>
+        <div class="Titulo_index">
+        <h1>Bienvenida/o al área de administración del centro</h1>
         <h3>Usa el menú superior para acceder a las distintas secciones de trabajo</h3>
+        </div>
     </main>
 
     <?php require "footer.php"; ?>
