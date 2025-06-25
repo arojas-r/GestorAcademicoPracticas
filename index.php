4805e6bc201b1f774b7a41355198a9fe3dd10dbd
Javier Cecilia Ruiz <laforjastore@gmail.com>
2025-06-25 17:37:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home INSTITUTO</title>
-    <link rel="stylesheet" href="instituto.css">
+    <link rel="stylesheet" href="css/instituto.css">
 </head>
 <?php require "header.php"; ?>
 
